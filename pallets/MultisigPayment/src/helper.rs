3a00438147129ea13fd042a3bb8062d5a0c7f99a
mrisholukamba <abdulrazzaqlukamba@gmail.com>
2022-10-05 21:52:51 +0300

    multi_account

--- pallets/MultisigPayment/src/helper.rs
+++ pallets/MultisigPayment/src/helper.rs
@@ -10,7 +10,9 @@
 use super::pallet::*;
 use frame_system::pallet_prelude::*;
 use frame_support::pallet_prelude::*;
-use sp_runtime::{traits::{TrailingZeroInput}
+use sp_runtime::{
+	MultiAddress,
+	traits::{TrailingZeroInput}
 };
 use codec::{Encode, Decode};
 
@@ -28,16 +30,6 @@ pub mod utils{
 		seller: AccountFor<T>,
 		resolver: Option<Resolver<T>>,
 	}
-	/// The full account information for a particular account ID.
-	#[pallet::storage]
-	#[pallet::getter(fn account)]
-	pub type Account<T: Config> = StorageMap<
-		_,
-		Blake2_128Concat,
-		T::AccountId,
-		AccountInfo<T::Index, T::AccountData>,
-		ValueQuery,
-	>;
 
 
 	// This will act as a dispute resolution methods. A user will have to choose which method
@@ -101,12 +93,6 @@ pub mod utils{
 
 		pub(crate) fn create_multi_account(multi_id: T::AccountId) -> DispatchResult{
 
-			let account_signer = AccountSigners::new{
-				buyer: "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY",
-				seller: "5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty",
-				resolver:Some("5FngjPYz7yGqrPZ8cvd1jPFS3tpTSqErjS7377Kw3izE4DMZ"),
-			 } ;
-			 let result = Self::derive_multi_id(account_signer);
 
 			Ok(())
 		}
